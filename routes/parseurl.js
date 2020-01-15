b6938bb7ce881ce2f2e23d6b3b80936e2d9dda29
leihan <han.lei@mail.hypers.com>
2020-01-15 14:59:18 +0800

    支持多URL解析

--- routes/parseurl.js
+++ routes/parseurl.js
@@ -2,42 +2,42 @@ const express = require('express');
 const router = express.Router();
 const jsdom = require("jsdom");
 const Readability = require('../lib/Readability')
-const {JSDOM} = jsdom;
+const { JSDOM } = jsdom;
 const phantom = require('phantom');
 
 function delay(second) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, second * 1000);
-    });
+  return new Promise((resolve) => {
+    setTimeout(resolve, second * 1000);
+  });
 };
-async function asyncPrint(url,res) {
-    const instance = await phantom.create();
-    const page = await instance.createPage();
-    const status = await page.open(url);
-    if(status==='success'){
-        await delay(3);//等待js异步加载3秒
-        let html = await page.property('content');//整个网页的html
-        // console.log(html)
-        let {document} = (new JSDOM(html)).window;
-        let documentJson = new Readability(document).parse();
-        if(documentJson && documentJson.content){
-            res.send(documentJson);
-        }else{
-            res.status(500).json({ error: '解析失败' });
-        }
-    }else{
-        res.status(400).json({ error: 'url参数错误' });
-    }
+async function asyncPrint(url) {
+  const instance = await phantom.create();
+  const page = await instance.createPage();
+  const status = await page.open(url);
+  let documentJson;
 
-    await instance.exit();
+  if (status === 'success') {
+    await delay(3);//等待js异步加载3秒
+    let html = await page.property('content');//整个网页的html
+    let { document } = (new JSDOM(html)).window;
+    documentJson = new Readability(document).parse();
+  } else {
+    documentJson = { error: 'url参数错误' }
+  }
 
+  await instance.exit();
+  return documentJson
 };
 
 router.get('/', (req, res, next) => {
-    (async function () {
-        let url = req.query.url;
-        await asyncPrint(url,res);
-    }())
+  (async function () {
+    let urls = req.query.url.split(',');
+    const ret = []
+    for (let url of urls) {
+      ret.push(await asyncPrint(url))
+    }
+    res.send(ret);
+  }())
 });
 
 module.exports = router;
